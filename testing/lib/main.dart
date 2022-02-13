85ab99bac8fb14ed9cc3b871c0d418b86b4cc31d
Aditya Kanikdaley <adikanikdaley@gmail.com>
2022-02-13 20:22:22 +0530

    first commit

---
+++ testing/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Name here'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          children: const <Widget>[Text('Add Widgets Here')],
+        ),
+      ),
+    );
+  }
+}
